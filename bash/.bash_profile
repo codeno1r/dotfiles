7f53624bc53129aa40b526c073e4d0bfb05aa49f
codeno1r <gvarrworks@gmail.com>
2025-06-26 09:14:27 +0800

    update hyprland in my pc

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -3,15 +3,14 @@
 #
 
 export XDG_CONFIG_HOME="$HOME/.config"
-export QT_QPA_PLATFORMTHEME=qt5ct
+# export QT_QPA_PLATFORMTHEME=qt5ct
 export QT_QPA_PLATFORMTHEME=qt6ct
 export PATH=$HOME/.local/bin:$PATH
 export PATH=/opt/WebWork\ Tracker:$PATH
-[[ -f ~/.bashrc ]] && . ~/.bashrc
 
-startx
+# startx
 
-# # Start Hyprland automatically on TTY1
-# if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
-#   exec Hyprland
-# fi
+# Start Hyprland automatically on TTY1
+if [[ -z $DISPLAY ]] && [[ $(tty) == /dev/tty1 ]]; then
+  exec Hyprland
+fi
--- dwm/.config/dwm/config.h
+++ dwm/.config/dwm/config.h
@@ -1,19 +1,20 @@
 /* See LICENSE file for copyright and license details. */
+
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx = 3; /* border pixel of windows */
-static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappih = 5;   /* horiz inner gap between windows */
-static const unsigned int gappiv = 5;   /* vert inner gap between windows */
-static const unsigned int gappoh =
-    10; /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov =
-    10; /* vert outer gap between windows and screen edge */
-static int smartgaps =
-    0; /* 1 means no outer gap when there is only one window */
+static const unsigned int borderpx = 4;
+static const unsigned int snap = 5;   /* snap pixel */
+static const unsigned int gappih = 5; /* horiz inner gap between windows */
+static const unsigned int gappiv = 5; /* vert inner gap between windows */
+static const unsigned int gappoh = 5; /* horiz outer gap */
+static const unsigned int gappov = 5; /* vert outer gap */
+static int smartgaps = 0;
+/* 1 means no outer gap when there is only one window */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
+static const int vertpad = 0; /* vertical padding of bar */
+static const int sidepad = 0; /* horizontal padding of bar */
 static const char *fonts[] = {"JetBrainsMono Nerd Font:style=Bold:size=14",
                               "Noto Color Emoji:size=12",
                               "NotoSans Nerd Font:size=12"};
@@ -34,7 +35,7 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char *tags[] = {"⛩", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -42,22 +43,18 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    {"steam_app_381210", NULL, NULL, 1, 0, 0},
-    {"Code", NULL, NULL, 1 << 1, 0, 0},
+    {"firefox", NULL, NULL, 1 << 1, 0, 0},
     {"Google-chrome", NULL, NULL, 1 << 2, 0, 0},
     {"Brave-browser", NULL, NULL, 1 << 2, 0, 0},
     {"Roam", NULL, NULL, 1 << 3, 0, 0},
-    {"zoom", NULL, NULL, 1 << 5, 0, 0},
-    {"WebWork Tracker", NULL, NULL, 1 << 8, 1, 0},
+    {"discord", NULL, NULL, 1 << 4, 0, 0},
+    {"ZapZap", NULL, NULL, 1 << 5, 0, 0},
     {"mpv", NULL, NULL, 1 << 6, 0, 0},
+    {"zoom", NULL, NULL, 1 << 7, 0, 0},
     {"Virt-manager", NULL, NULL, 1 << 7, 0, 0},
     {"Virt-viewer", NULL, NULL, 1 << 7, 0, 0},
-    {"steam", NULL, NULL, 1 << 7, 0, 0},
+    {"WebWork Tracker", NULL, NULL, 1 << 8, 1, 0},
 
-    {"firefox", NULL, NULL, 1 << 2, 0, 1},
-    {"WhatSie", NULL, NULL, 1 << 3, 0, 1},
-    {"ZapZap", NULL, NULL, 1 << 3, 0, 1},
-    {"discord", NULL, NULL, 1 << 3, 0, 1},
 };
 
 /* layout(s) */
@@ -105,31 +102,49 @@ static const Layout layouts[] = {
     .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
   }
 
-#define STATUSBAR "dwmblocks"
-
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"dmenu_run", "-c", NULL};
-static const char *termcmd[] = {"st", NULL};
+static const char *termcmd[] = {"alacritty", NULL};
 static const char *filemgrcmd[] = {"thunar", NULL};
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
-    {MODKEY, XK_space, spawn, {.v = dmenucmd}},
+
+    /********************
+    **General Shortcuts**
+    ********************/
+
+    // Tags
+    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
+        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
+            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+
+    // Terminal
     {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
+
+    // Menu
+    {MODKEY, XK_space, spawn, {.v = dmenucmd}},
+
+    // Switch App Position
+    {MODKEY, XK_Return, zoom, {0}},
+
+    // View last App
+    {MODKEY, XK_Tab, view, {0}},
+
+    // Close App
+    {MODKEY | ShiftMask, XK_c, killclient, {0}},
+
     {MODKEY, XK_b, togglebar, {0}},
     {MODKEY, XK_e, spawn, {.v = filemgrcmd}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_d, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
     {MODKEY | ShiftMask, XK_h, setcfact, {.f = +0.25}},
     {MODKEY | ShiftMask, XK_l, setcfact, {.f = -0.25}},
     {MODKEY | ShiftMask, XK_o, setcfact, {.f = 0.00}},
-    {MODKEY, XK_Return, zoom, {0}},
     {MODKEY | Mod4Mask, XK_u, incrgaps, {.i = +1}},
     {MODKEY | Mod4Mask | ShiftMask, XK_u, incrgaps, {.i = -1}},
     {MODKEY | Mod4Mask, XK_i, incrigaps, {.i = +1}},
@@ -146,30 +161,49 @@ static const Key keys[] = {
     {MODKEY | Mod4Mask | ShiftMask, XK_9, incrovgaps, {.i = -1}},
     {MODKEY | Mod4Mask, XK_0, togglegaps, {0}},
     {MODKEY | Mod4Mask | ShiftMask, XK_0, defaultgaps, {0}},
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY | ShiftMask, XK_c, killclient, {0}},
-    //	{ MODKEY,                       XK_t,      setlayout,      {.v =
-    //&layouts[0]} }, 	{ MODKEY,                       XK_f,      setlayout,
-    //{.v = &layouts[1]} }, 	{ MODKEY,                       XK_m, setlayout,
-    //{.v = &layouts[2]} }, 	{ MODKEY,                       XK_space,
-    // setlayout, {0} }, 	{ MODKEY|ShiftMask,             XK_space,
-    // togglefloating, {0} },
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
+    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
     {MODKEY, XK_comma, focusmon, {.i = -1}},
     {MODKEY, XK_period, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
+
+    // {MODKEY, XK_i, incnmaster, {.i = +1}},
+    // {MODKEY, XK_d, incnmaster, {.i = -1}},
+    // {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
+    // {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
+    // {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
+    // {MODKEY, XK_space, setlayout, {0}},
+    // {MODKEY, XK_0, view, {.ui = ~0}},
+    // {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
+
     // {MODKEY, XK_minus, setgaps, {.i = -1}},
     // {MODKEY, XK_equal, setgaps, {.i = +1}},
     // {MODKEY | ShiftMask, XK_equal, setgaps, {.i = 0}},
-    {MODKEY, XK_F7, spawn, SHCMD("kill -49 $(pidof dwmblocks); nightlight.sh")},
+
+    // Custom Shortcuts
+    {MODKEY | ShiftMask, XK_s, spawn,
+     SHCMD("flameshotgui-dwm.sh")}, // If you're using DWM
+    {MODKEY | ShiftMask, XK_b, spawn, SHCMD("WebWorkTracker.sh")},
     {MODKEY | ShiftMask, XK_n, spawn, SHCMD("nlchangestatus.sh")},
-    {MODKEY | ShiftMask, XK_s, spawn, SHCMD("flameshotgui-dwm.sh")},
     {MODKEY | ShiftMask, XK_m, spawn, SHCMD("win10.sh")},
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+
+    // // Lenovo Laptop Custom Shortcut
+    // {MODKEY, XK_F7, spawn, SHCMD("kill -49 $(pidof dwmblocks);
+    // nightlight.sh")}, {0, XF86XK_AudioMute, spawn,
+    //  SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof "
+    //        "dwmblocks)")},
+    // {0, XF86XK_AudioRaiseVolume, spawn,
+    //  SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+; kill -44 $(pidof "
+    //        "dwmblocks)")},
+    // {0, XF86XK_AudioLowerVolume, spawn,
+    //  SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-; kill -44 $(pidof "
+    //        "dwmblocks)")},
+    // {0, XF86XK_MonBrightnessUp, spawn,
+    //  SHCMD("brightnessctl s 10%+; kill -48 $(pidof dwmblocks)")},
+    // {0, XF86XK_MonBrightnessDown, spawn,
+    //  SHCMD("brightnessctl s 10%-; kill -48 $(pidof dwmblocks)")},
+
+    // PC Custom Shortcut
     {MODKEY | ShiftMask, XK_F10, spawn,
      SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof "
            "dwmblocks)")},
@@ -179,23 +213,19 @@ static const Key keys[] = {
     {MODKEY | ShiftMask, XK_F11, spawn,
      SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-; kill -44 $(pidof "
            "dwmblocks)")},
-    {0, XF86XK_MonBrightnessUp, spawn,
-     SHCMD("brightnessctl s 10%+; kill -48 $(pidof dwmblocks)")},
-    {0, XF86XK_MonBrightnessDown, spawn,
-     SHCMD("brightnessctl s 10%-; kill -48 $(pidof dwmblocks)")},
+
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
  * ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
-    /* click                event mask      button          function argument */
+    /* click                event mask      button          function
+       argument */
     {ClkLtSymbol, 0, Button1, setlayout, {0}},
     {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
     {ClkWinTitle, 0, Button2, zoom, {0}},
-    {ClkStatusText, 0, Button1, sigstatusbar, {.i = 1}},
-    {ClkStatusText, 0, Button2, sigstatusbar, {.i = 2}},
-    {ClkStatusText, 0, Button3, sigstatusbar, {.i = 3}},
+    {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
     {ClkClientWin, MODKEY, Button1, movemouse, {0}},
     {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
     {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
