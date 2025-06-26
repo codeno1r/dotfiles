f1f832853ff622a189a631b8fe957ed1c6dd61d7
codeno1r <gvarrworks@gmail.com>
2025-06-26 09:14:27 +0800

    update hyprland in my pc

--- bash/.bashrc
+++ bash/.bashrc
@@ -11,3 +11,4 @@ alias ff="fastfetch"
 alias yi="yazi"
 alias inv='interactivenvim.sh'
 alias deltemp='sudo pacman -Rns $(pacman -Qdtq)'
+alias mdb="mongosh mongodb+srv://cluster0.xy0sq.mongodb.net/ptadmin --apiVersion 1 --username gvarrworks --password A1APwwOLA2Z2O5ks"
--- dwm/.config/bak.dwm/config.def.h
+++ dwm/.config/bak.dwm/config.def.h
@@ -2,33 +2,22 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx = 2; /* border pixel of windows */
-static const unsigned int gappx = 10;   /* gaps between windows */
+static const unsigned int borderpx = 3; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int systraypinning =
-    0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
-          X */
-static const unsigned int systrayonleft =
-    0; /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 10; /* systray spacing */
-static const int systraypinningfailfirst =
-    1; /* 1: if pinning fails, display systray on the first monitor, False:
-          display systray on the last monitor*/
-static const int showsystray = 0; /* 0 means no systray */
-static const int showbar = 1;     /* 0 means no bar */
-static const int topbar = 1;      /* 0 means bottom bar */
-static const int vertpad = 0;     /* vertical padding of bar */
-static const int sidepad = 0;     /* horizontal padding of bar */
+static const unsigned int gappih = 5;   /* horiz inner gap between windows */
+static const unsigned int gappiv = 5;   /* vert inner gap between windows */
+static const unsigned int gappoh =
+    10; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov =
+    10; /* vert outer gap between windows and screen edge */
+static int smartgaps =
+    0; /* 1 means no outer gap when there is only one window */
+static const int showbar = 1; /* 0 means no bar */
+static const int topbar = 1;  /* 0 means bottom bar */
 static const char *fonts[] = {"JetBrainsMono Nerd Font:style=Bold:size=14",
                               "Noto Color Emoji:size=12",
                               "NotoSans Nerd Font:size=12"};
 static const char dmenufont[] = "JetBrainsMono Nerd Font:style=Bold:size=14";
-static const char col_gray1[] = "#09090b";
-static const char col_gray2[] = "#1e2939";
-static const char col_gray3[] = "#52525c";
-static const char col_gray4[] = "#99a1af";
-static const char col_gray5[] = "#e4e4e7";
-static const char col_cyan[] = "#0f172b";
 
 // 🌿 Elegant & Relaxing DWM Color Scheme
 // static const char dark_slate[] = "#2E3440";
@@ -79,11 +68,27 @@ static const int resizehints =
 static const int lockfullscreen =
     1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT                                                           \
+  1 /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
     /* symbol     arrange function */
     {"[]=", tile}, /* first entry is default */
-    {"><>", NULL}, /* no layout function means floating behavior */
     {"[M]", monocle},
+    {"[@]", spiral},
+    {"[\\]", dwindle},
+    {"H[]", deck},
+    {"TTT", bstack},
+    {"===", bstackhoriz},
+    {"HHH", grid},
+    {"###", nrowgrid},
+    {"---", horizgrid},
+    {":::", gaplessgrid},
+    {"|M|", centeredmaster},
+    {">M>", centeredfloatingmaster},
+    {"><>", NULL}, /* no layout function means floating behavior */
+    {NULL, NULL},
 };
 
 /* key definitions */
@@ -105,20 +110,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
-// static const char *dmenucmd[] = {
-//     "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb", col_gray1,
-//     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *dmenucmd[] = {"dmenu_run", "-c", NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *filemgrcmd[] = {"thunar", NULL};
-// static const char *keepassxccmd[] = {"keepassxc", NULL};
-// static const char *bluemanApplet[] = {"blueman-applet", NULL};
-// static const char *nmApplet[] = {"nm-applet", NULL};
-// static const char *noisetorch[] = {"noisetorch", "-i", NULL};
-
-Autostarttag autostarttaglist[] = {
-    {.cmd = NULL, .tags = 0},
-};
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
@@ -132,7 +126,26 @@ static const Key keys[] = {
     {MODKEY, XK_d, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
+    {MODKEY | ShiftMask, XK_h, setcfact, {.f = +0.25}},
+    {MODKEY | ShiftMask, XK_l, setcfact, {.f = -0.25}},
+    {MODKEY | ShiftMask, XK_o, setcfact, {.f = 0.00}},
     {MODKEY, XK_Return, zoom, {0}},
+    {MODKEY | Mod4Mask, XK_u, incrgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_u, incrgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_i, incrigaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_i, incrigaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_o, incrogaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_o, incrogaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_6, incrihgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_6, incrihgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_7, incrivgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_7, incrivgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_8, incrohgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_8, incrohgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_9, incrovgaps, {.i = +1}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_9, incrovgaps, {.i = -1}},
+    {MODKEY | Mod4Mask, XK_0, togglegaps, {0}},
+    {MODKEY | Mod4Mask | ShiftMask, XK_0, defaultgaps, {0}},
     {MODKEY, XK_Tab, view, {0}},
     {MODKEY | ShiftMask, XK_c, killclient, {0}},
     //	{ MODKEY,                       XK_t,      setlayout,      {.v =
@@ -147,9 +160,9 @@ static const Key keys[] = {
     {MODKEY, XK_period, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    {MODKEY, XK_minus, setgaps, {.i = -1}},
-    {MODKEY, XK_equal, setgaps, {.i = +1}},
-    {MODKEY | ShiftMask, XK_equal, setgaps, {.i = 0}},
+    // {MODKEY, XK_minus, setgaps, {.i = -1}},
+    // {MODKEY, XK_equal, setgaps, {.i = +1}},
+    // {MODKEY | ShiftMask, XK_equal, setgaps, {.i = 0}},
     {MODKEY, XK_F7, spawn, SHCMD("kill -49 $(pidof dwmblocks); nightlight.sh")},
     {MODKEY | ShiftMask, XK_n, spawn, SHCMD("nlchangestatus.sh")},
     {MODKEY | ShiftMask, XK_s, spawn, SHCMD("flameshotgui-dwm.sh")},
@@ -157,13 +170,13 @@ static const Key keys[] = {
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
             TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
-    {MODKEY | ShiftMask, XK_F10, spawn,
+    {0, XF86XK_AudioMute, spawn,
      SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof "
            "dwmblocks)")},
-    {MODKEY | ShiftMask, XK_F12, spawn,
+    {0, XF86XK_AudioRaiseVolume, spawn,
      SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+; kill -44 $(pidof "
            "dwmblocks)")},
-    {MODKEY | ShiftMask, XK_F11, spawn,
+    {0, XF86XK_AudioLowerVolume, spawn,
      SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-; kill -44 $(pidof "
            "dwmblocks)")},
     {0, XF86XK_MonBrightnessUp, spawn,
